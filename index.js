1298b86c28977db133adfc89f7acf17247f24f27
Sahadev Bhaganagare <sahadevbh8@gmail.com>
2022-10-06 19:59:47 +0530

    new modification

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const http = require("http");
 const fs = require("fs");
-fs.unlink("test.txt",
-    (err) => {
-        if(err) throw err;
-        console.log("File test.txt deleted successfully");
-    });
+const server = http.createServer((req,res) => {
+    const stream = fs.createReadStream("sample.txt");
+    stream.pipe(res)
+    /*fs.readFile("sample.txt", (err, data) => {
+        res.end(data);
+    })*/
+});
+server.listen(3000);
